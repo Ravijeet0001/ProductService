c166932a3bbf70d928224a6a7465a3854cd70286
Ravijeet choudhary <ravijeet.choudhary.me.2018@miet.ac.in>
2025-12-22 20:30:26 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/scaler/productservicedec25/ProductServiceDec25Application.java
@@ -0,0 +1,13 @@
+package com.scaler.productservicedec25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceDec25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceDec25Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/productservicedec25/ProductServiceDec25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.productservicedec25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceDec25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
