7e22eb2f27b62da209699801862c46f2648cc9e9
Ravijeet choudhary <ravijeet.choudhary.me.2018@miet.ac.in>
2025-12-26 17:25:04 +0530

    Add base model and product/category entities with initial controller setup

---
+++ src/main/java/com/scaler/productservicedec25/models/Category.java
@@ -0,0 +1,15 @@
+package com.scaler.productservicedec25.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Category extends BaseModel {
+
+    private String name;
+    private String description;
+
+
+}
---
+++ src/main/java/com/scaler/productservicedec25/models/Product.java
@@ -0,0 +1,15 @@
+package com.scaler.productservicedec25.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product extends BaseModel {
+
+    private String title;
+    private double price;
+    private Category category;
+
+}
